30ff955cab651a389e492a0b5d3844647a17443a
Lucas <lucas@agtlucas.com>
2016-07-10 21:02:18 -0300

    Improve code

--- test/index.js
+++ test/index.js
@@ -3,17 +3,17 @@ const expect = require('chai').expect
 const plugin = require('../')
 
 const test = (input, output, opts, done) => {
-  postcss([plugin(opts)]).process(input).then((result) => {
+  postcss([plugin(opts)]).process(input).then(result => {
     expect(result.css).to.eql(output)
     expect(result.warnings()).to.be.empty
     done()
-  }).catch((error) => {
+  }).catch(error => {
     done(error)
   })
 }
 
 describe('postcss-bg', () => {
-  it('Should replace `bg` with `background`', (done) => {
+  it('Should replace `bg` with `background`', done => {
     test('.blackbriar { bg: #666 }', '.blackbriar { background: #666 }', {}, done)
   })
 })
