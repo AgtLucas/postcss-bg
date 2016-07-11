13c5262bb4f8b72f88505769bbb05f35d03226d9
Lucas <lucas@agtlucas.com>
2016-07-10 21:01:40 -0300

    Improve code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-bg', () => {
-  return (css) => {
-    css.walkDecls('bg', (decl) => {
+  return css => {
+    css.walkDecls('bg', decl => {
       decl.prop = 'background'
     })
   }
